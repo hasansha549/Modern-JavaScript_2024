e664745865d4c356ac123ddce60a7513d2fe3ba6
Mahmudul Hasan <idbtechnology46@gmail.com>
2026-04-24 20:28:58 +0600

    Add files via upload

---
+++ 07_JS_Data_Types/script.js
@@ -0,0 +1,40 @@
+/* JavaScript Data Type */
+
+let fristName = "Shahriar Alli";
+fristName = "Rohit Khan"
+// let fullName = 'My Name is \'Mahmudul Hasan\'';
+let fullName = "My Name is 'Mahmudul Hasan'";
+let fainalName = `May Name is ${fristName}`;
+
+
+let age = 30;
+let age2 = fristName + 5 + 10;
+let Calculation = age + age2 + fristName + age + age2;
+
+let = numberBig = 9n;
+let = numberBig2 = BigInt(age);
+
+
+let ifAdult = false;
+
+let someInfo = null;
+
+let fruite = ['Apple', 'Lemon', 'Peer', 'Oranges', 32, true.undefind, null];
+fruite[4] = 'Mango';
+
+
+let student = {
+    fullName: 'Asad Khan',
+    age: 32,
+    dateOfBirth: 1932
+}
+console.log(fainalName);
+console.log(student);
+console.log(typeof student);
+
+/**
+ * !sdjh
+ * ?kej
+ * TODO:
+ */
+
