9c3661f0d4920d256fd69a2cba52bd4cf5f7bd33
Mahmudul Hasan <idbtechnology46@gmail.com>
2026-04-24 19:38:03 +0600

    Add files via upload

---
+++ 01_JS_Environment_Setup/script.js
@@ -0,0 +1,7 @@
+// console of 3 type For Example
+console.log('Hello');
+console.error('Invalid difficulty level. Choose "easy", "medium", or "hard"');
+console.dirxml()
+console.dir()
+document.getElementById('title').innerHTML = 'Hello world';
+
