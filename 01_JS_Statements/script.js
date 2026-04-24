c3deabe82f5e8280a83f22a732062cbde1ad3f20
Mahmudul Hasan <idbtechnology46@gmail.com>
2026-04-24 19:54:00 +0600

    Add files via upload

---
+++ 01_JS_Statements/script.js
@@ -0,0 +1,14 @@
+let firstName = 'Mahmud';
+let age = 30;
+
+firstName = 'Emon'
+
+
+console.log(firstName);
+console.log(age + 40);
+
+function funcName() {
+    console.log('My First Funtion');
+}
+
+funcName()
