268649392f06ded6e3fb9b86a007b229af130cfb
Mahmudul Hasan <idbtechnology46@gmail.com>
2026-04-24 20:17:17 +0600

    Add files via upload

---
+++ 04_JS_Comments/script.js
@@ -0,0 +1,25 @@
+//My First Comment
+
+let firstName = 'Mahmud';
+let age = 30;
+
+/*
+ * Multi Line Comment
+? Some info
+! Warning
+TODO: Some thing i need to fix
+ */
+
+
+firstName = 'Emon'   //Change firstName 
+
+
+console.log(firstName);
+console.log(age + 40);
+/*
+function funcName() {
+    console.log('My First Funtion');
+}
+
+funcName();
+*/
