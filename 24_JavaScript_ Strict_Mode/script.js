9c55d72ea3979f540bed51a3dd8512b40fdf7c6d
Mahmudul Hasan <idbtechnology46@gmail.com>
2026-04-25 20:51:33 +0600

    Add files via upload

---
+++ 24_JavaScript_ Strict_Mode/script.js
@@ -0,0 +1,25 @@
+// JavaScript Strict Mode
+
+// 'use strict';
+
+let x = 6;
+console.log(x);
+
+earningZone();
+function earningZone() {
+    let y = 9;
+    console.log(y);
+}
+
+microsot(5, 10);
+function microsot(x1, y1) {
+    'use strict';               // Strict mode in Function, Efect in only Function Ara
+    let y = 9;
+    console.log(x1, y1);
+}
+
+
+// let private = 'Some text';
+// let public = 'Some text';
+// let package = 'Some text';
+
